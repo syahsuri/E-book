1cc0b7c12f669151b54fe547ea652bf2a1a99552
syahsuri <officialsyahsury@gmail.com>
2022-09-10 14:44:32 +0700

    E-book fisrt step

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p class="text-center">Copyright &copy 2022 TanyoMerno</p>
+</footer>
---
+++ resources/views/layout/navbar.blade.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg">
+   <div class="container">
+   <a class="navbar-brand fw-bold" href="#">TANYO <span>MERNO</span></a>
+   <ul class="nav justify-content-end">
+   <li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Username</a>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Profile</a></li>
+      <li><a class="dropdown-item" href="#">Logout</a></li>
+    </ul>
+  </li>
+
+   </ul>
+   </div>
+</nav>
